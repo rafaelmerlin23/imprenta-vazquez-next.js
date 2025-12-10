493472a4760fbad1f78d1fdb75bdbb2ac16cf329
Firepants69 <rafaelmerlinprieto@gmail.com>
2025-12-10 15:44:42 -0600

    deploy on nginx

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: '/app',
   output: 'standalone',
   images: { unoptimized: true },
   trailingSlash: false,
-  basePath: '/app',
   typescript: { ignoreBuildErrors: true },
+  async redirects() {
+    return [
+      {
+        source: '/app',
+        destination: '/app/',
+        permanent: true,
+      },
+    ];
+  },
 };
-
 export default nextConfig;

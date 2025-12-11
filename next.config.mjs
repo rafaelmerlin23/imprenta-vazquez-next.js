9e377549c4f44b18a557202c0af691750d1bd74b
Firepants69 <rafaelmerlinprieto@gmail.com>
2025-12-11 10:29:56 -0600

    deploy config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/app",
-  assetPrefix: "/app",
   output: "standalone",
   images: { unoptimized: true },
   trailingSlash: true,
